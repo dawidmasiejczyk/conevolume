b1e86cc2b9e1d6059e426f8d09eb39bb42b406a6
dmas <dmas@wp.pl>
2019-12-19 12:40:37 +0100

    initial commit - full poject

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double Pi = 3.14;
+	    int liczbaA = 4;
+	    int h = 6;
+	    double objetoscStozka;
+
+	    objetoscStozka = (Pi * liczbaA * liczbaA * h)/3  ;
+        System.out.println(objetoscStozka);
+    }
+}
